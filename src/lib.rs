36ef7408b0e1f812488e6eb9a9172583fe14995a
Nazeer Greene <nazeerg5@gmail.com>
2024-08-29 22:14:21 -0700

    created simple to-do list Task data structure; outlined DS to hold tasks

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,38 @@
 pub mod fillers;
+
+pub mod ToDo {
+
+    struct Task {
+        complete: bool,
+        task: String,
+    }
+
+    impl Task {
+        fn new() -> Self {
+            Task {
+                complete: false,
+                task: String::new(),
+            }
+        }
+
+        fn from(complete: bool, task: String) -> Self {
+            Task { complete, task }
+        }
+    } // Task
+
+    /*  data structure to hold tasks
+
+        TaskList:
+            vector: Task
+
+            methods:
+                init
+                    - new empty vector
+                    - vector from list of tasks
+                    - vector from another list
+                print tasks
+                add tasks
+                remove tasks
+                toggle task completetion
+    */
+}
