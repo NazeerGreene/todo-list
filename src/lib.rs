76d829d21e99374d0ad9221f7b938646b4b8aea8
Nazeer Greene <nazeerg5@gmail.com>
2024-08-29 23:14:22 -0700

    finished basic crud operations: add, remove, and toggle completion for items in list

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,25 @@
 pub mod fillers;
 
-pub mod ToDo {
+pub mod to_do {
 
-    struct Task {
+    pub struct Task {
         complete: bool,
         task: String,
     }
 
     impl Task {
-        fn new() -> Self {
-            Task {
+        pub fn new() -> Self {
+            Self {
                 complete: false,
                 task: String::new(),
             }
         }
 
-        fn from(complete: bool, task: String) -> Self {
-            Task { complete, task }
+        pub fn from(complete: bool, task: &str) -> Self {
+            Self {
+                complete,
+                task: String::from(task),
+            }
         }
     } // Task
 
@@ -27,12 +30,48 @@ pub mod ToDo {
 
             methods:
                 init
-                    - new empty vector
-                    - vector from list of tasks
-                    - vector from another list
-                print tasks
-                add tasks
-                remove tasks
-                toggle task completetion
+                    - new empty vector -- done
+                    - vector from list of tasks -- add when needed
+                    - vector from another list  -- add when needed
+                enable iterator for tasks
+                add tasks -- done
+                remove tasks -- done
+                toggle task completetion -- done
     */
+    pub struct ToDoList {
+        list: Vec<Task>,
+    }
+
+    impl ToDoList {
+        pub fn new() -> Self {
+            Self {
+                list: Vec::with_capacity(10), // arbitrary capacity
+            }
+        }
+
+        pub fn add(&mut self, task: Task) {
+            self.list.push(task);
+        }
+
+        pub fn remove(&mut self, idx: usize) -> Option<Task> {
+            if idx <= self.list.capacity() {
+                Some(self.list.remove(idx))
+            } else {
+                None
+            }
+        }
+
+        pub fn toggle_task(&mut self, idx: usize) {
+            if let Some(task) = self.list.get_mut(idx) {
+                task.complete = !task.complete;
+            }
+        }
+
+        // testing
+        pub fn print(&self) {
+            for item in self.list.iter() {
+                println!("[{}] {}", if item.complete { "X" } else { " " }, item.task);
+            }
+        }
+    } // ToDoList
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
 use todo_list::fillers::{todo, Thing};
+use todo_list::to_do::{Task, ToDoList};
 
 fn main() {
-    todo();
+    let mut first = Task::from(false, "finish homework");
+
+    let mut mylist = ToDoList::new();
+    mylist.add(first);
+
+    mylist.add(Task::from(true, "do the dishes"));
+
+    mylist.print();
+
+    mylist.toggle_task(1);
+
+    mylist.print();
+
+    mylist.toggle_task(0);
+
+    mylist.print();
 }
